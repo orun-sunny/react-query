b4d0ea8d6d3dcb0e3eedc727f39c58721e8d2c3a
orun sunny <sunny.mukut@gmail.com>
2024-02-20 12:08:23 +0600

    first commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const retrivedProducts = async ({ queryKey }) => {
+  //   console.log(obj);
+  const response = await axios.get(`http://localhost:3000/${queryKey[0]}`);
+  return response.data;
+};
+
+const ProductList = () => {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: retrivedProducts,
+  });
+  //   if (products) {
+  //     console.log("Products data:", products);
+  //   }
+  if (isLoading) return <div>Fetching Products ....</div>;
+  if (error) return <div>An error occured: {error.message} </div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-3/5">
+      <h2 className="text-3xl my-2">Product List</h2>
+      <ul className="flex flex-wrap justify-center items-center">
+        {products &&
+          products.map((product) => (
+            <li
+              key={product.id}
+              className="flex flex-col items-center m-2 border rounded-sm"
+            >
+              <img
+                className="object-cover h-64 w-96 rounded-sm"
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <p className="text-xl my-3">{product.title}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
