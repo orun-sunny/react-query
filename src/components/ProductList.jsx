74c2799fdbc329b6cec3c4e7ca74ba8ecb245cf2
orun sunny <sunny.mukut@gmail.com>
2024-02-20 15:41:16 +0600

    first commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,32 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useState } from "react";
 
-const retrivedProducts = async ({ queryKey }) => {
-  //   console.log(obj);
-  const response = await axios.get(`http://localhost:3000/${queryKey[0]}`);
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+const retrieveProducts = async ({ queryKey }) => {
+  const response = await axios.get(
+    `http://localhost:3000/products?_page=${queryKey[1].page}&_per_page=6`
+  );
   return response.data;
 };
 
 const ProductList = () => {
+  const [page, setPage] = useState(1);
   const {
     data: products,
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["products"],
-    queryFn: retrivedProducts,
+    queryKey: ["products", { page }],
+    queryFn: retrieveProducts,
   });
-  //   if (products) {
-  //     console.log("Products data:", products);
-  //   }
-  if (isLoading) return <div>Fetching Products ....</div>;
-  if (error) return <div>An error occured: {error.message} </div>;
+
+  if (isLoading) return <div>Fetching Products...</div>;
+  if (error) return <div>An error occured: {error.message}</div>;
+
   return (
     <div className="flex flex-col justify-center items-center w-3/5">
       <h2 className="text-3xl my-2">Product List</h2>
       <ul className="flex flex-wrap justify-center items-center">
-        {products &&
-          products.map((product) => (
+        {products.data &&
+          products.data.map((product) => (
             <li
               key={product.id}
               className="flex flex-col items-center m-2 border rounded-sm"
@@ -40,6 +43,26 @@ const ProductList = () => {
             </li>
           ))}
       </ul>
+      <div className="flex">
+        {products.prev && (
+          <button
+            className="p-1 mx-1 bg-gray-100 border cursor-pointer rounded-sm"
+            onClick={() => setPage(products.prev)}
+          >
+            {" "}
+            Prev{" "}
+          </button>
+        )}
+        {products.next && (
+          <button
+            className="p-1 mx-1 bg-gray-100 border cursor-pointer rounded-sm"
+            onClick={() => setPage(products.next)}
+          >
+            {" "}
+            Next{" "}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
